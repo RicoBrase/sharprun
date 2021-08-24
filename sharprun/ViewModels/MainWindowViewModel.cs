4981b9515eeeb7a626fcaae9faef43d4bc30b9c4
Rico Brase <kontakt@rico-brase.de>
2021-08-24 23:28:27 +0200

    UI: Added List and input handling.

--- sharprun/ViewModels/MainWindowViewModel.cs
+++ sharprun/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using ReactiveUI;
 
 namespace sharprun.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private string _command = "";
+        private int _selectedApp;
+        private int _selectedIndex = -1;
+        private bool _selectionChanged;
+
+        public string Command
+        {
+            get => _command;
+            set => this.RaiseAndSetIfChanged(ref _command, value);
+        }
+
+        public int SelectedApp
+        {
+            get => _selectedApp;
+            set
+            {
+                Command = Convert.ToString(value);
+                this.RaiseAndSetIfChanged(ref _selectedApp, value);
+            }
+        }
+
+        public int SelectedIndex
+        {
+            get => _selectedIndex;
+            set => this.RaiseAndSetIfChanged(ref _selectedIndex, value);
+        }
+
+        public List<int> Apps => Enumerable.Range(1, 10).ToList();
+
+        public void CloseApplication()
+        {
+            Environment.Exit(0);
+        }
+
+        public void SelectNext()
+        {
+            if (!_selectionChanged)
+            {
+                _selectionChanged = true;
+            }
+            SelectedIndex = (SelectedIndex + 1) % Apps.Count;
+        }
+
+        public void SelectPrevious()
+        {
+            if (!_selectionChanged)
+            {
+                SelectedIndex = 0;
+                _selectionChanged = true;
+            }
+            SelectedIndex = (SelectedIndex - 1 + Apps.Count) % Apps.Count;
+        }
     }
 }
--- sharprun/Views/MainWindow.axaml.cs
+++ sharprun/Views/MainWindow.axaml.cs
@@ -1,11 +1,19 @@
+using System;
+using System.ComponentModel;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using sharprun.ViewModels;
 
 namespace sharprun.Views
 {
     public partial class MainWindow : Window
     {
+
+        private TextBox? _commandInput;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -17,6 +25,28 @@ namespace sharprun.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            _commandInput = this.FindControl<TextBox>("CommandInput");
+        }
+
+        private void Window_OnKeyDown(object? sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape when DataContext is not null:
+                    ((MainWindowViewModel) DataContext).CloseApplication();
+                    break;
+                case Key.Down when DataContext is not null:
+                    ((MainWindowViewModel) DataContext).SelectNext();
+                    break;
+                case Key.Up when DataContext is not null:
+                    ((MainWindowViewModel) DataContext).SelectPrevious();
+                    break;
+            }
+        }
+
+        private void Window_OnOpened(object? sender, EventArgs e)
+        {
+            _commandInput?.Focus();
         }
     }
 }
